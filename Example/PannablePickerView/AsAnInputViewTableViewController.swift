e30ac1eeaca5ae6de3862a985db1b99f821c5bc1
Diego Cruz <diegocruz.c@gmail.com>
2018-02-09 16:08:52 -0500

    Updating Swift version and targets from projects

--- Example/PannablePickerView/AsAnInputViewTableViewController.swift
+++ Example/PannablePickerView/AsAnInputViewTableViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 import PannablePickerView
 
-class AsAnInputViewTableViewController: UITableViewController, UITextFieldDelegate{
+class AsAnInputViewTableViewController: UITableViewController{
     //MARK: - Properties
     //MARK: IBOutlets
-    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField?
     //MARK: Variables
-    lazy var pannablePickerView:UIView = {
+    public lazy var pannablePickerView:UIView = {
         let pickerView = PannablePickerView()
-        pickerView.frame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, 200)
-        pickerView.addTarget(self, action: "valueChanged:", forControlEvents: .ValueChanged)
+        pickerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200)
+        pickerView.addTarget(self, action: #selector(valueChanged(_:)), for: .valueChanged)
         pickerView.backgroundColor = UIColor(red: 0.180, green: 0.659, blue: 0.929, alpha: 1.00)
         pickerView.minValue = 2005
         pickerView.maxValue = 2015
@@ -26,22 +26,26 @@ class AsAnInputViewTableViewController: UITableViewController, UITextFieldDelega
         return pickerView
     }()
     
-    
-    //MARK: - View events methods
-    override func viewDidAppear(animated: Bool) {
+    //MARK: - Public methods
+    //MARK: View events
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        yearTextField.becomeFirstResponder()
+        yearTextField?.becomeFirstResponder()
     }
     
-    //MARK: - UITextField delegate methods
-    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+    //MARK: - Private methods
+    //MARK: Actions
+    @objc private func valueChanged(_ sender:PannablePickerView){
+        let valueText = String(format: "%0.0f", sender.value)
+        yearTextField?.text = "\(valueText)"
+    }
+}
+
+//MARK: - Delegate methods
+//MARK: UITextFieldDelegate
+extension AsAnInputViewTableViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         textField.inputView = pannablePickerView
         return true
-    }
-    
-    //MARK: - Action methods
-    func valueChanged(sender:PannablePickerView){
-        let valueText = String(format: "%0.0f", sender.value)
-        yearTextField.text = "\(valueText)"
     }
 }
