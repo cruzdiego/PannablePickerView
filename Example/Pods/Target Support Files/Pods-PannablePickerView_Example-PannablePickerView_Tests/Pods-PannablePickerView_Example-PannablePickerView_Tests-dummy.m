9653d1a21580c7558b419533e148dc9ab133442f
Diego Cruz <diegocruz.c@gmail.com>
2018-02-09 15:58:13 -0500

    Updating cocoapod version

---
+++ Example/Pods/Target Support Files/Pods-PannablePickerView_Example-PannablePickerView_Tests/Pods-PannablePickerView_Example-PannablePickerView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PannablePickerView_Example_PannablePickerView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PannablePickerView_Example_PannablePickerView_Tests
+@end
